947489c9edbdd43bcf64afb185b8ca13cb6f9c30
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2024-04-03 11:06:20 +0200

    Organised folders & added missing script icons

--- Assets/Scripts/Modules/StateManager.cs
+++ Assets/Scripts/Modules/StateManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PropStateManager : MonoBehaviour
+public class StateManager : MonoBehaviour
 {
     [Header("State")]
     [SerializeField] private bool _defaultState = false;
